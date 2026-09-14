8cbd0aaab2dbd5f95b29635f5277f838c6aa4a27
vijit-vishnoi <vishnoivijit@gmail.com>
2026-09-14 14:12:08 +0530

    Secure local secrets and add dotenv initialization

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
